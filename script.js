b712cdaede034c4b8fc2c906a117ae120509029a
niftydatahub <niftydatahub@gmail.com>
2026-05-12 10:22:36 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const stocks = [
+  {
+    symbol: "RELIANCE",
+    price: 2950,
+    change: 1.2,
+    high52: 3120,
+    low52: 2220
+  },
 
+  {
+    symbol: "TCS",
+    price: 4100,
+    change: -0.4,
+    high52: 4250,
+    low52: 3311
+  },
+
+  {
+    symbol: "INFY",
+    price: 1580,
+    change: 2.1,
+    high52: 1750,
+    low52: 1350
+  }
+];
+
+console.log(stocks);
